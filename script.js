769bfcc86f6d0f91a968a0fc78ee3088032224a3
zola107 <camillecolleauxpro123@gmail.com>
2025-11-06 13:49:41 +0000

    feat: Maitrise variables et condition

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Laboratoire prêt !");
+
+const nom = "Camille";
+let age = 18;
+
+
+const motDePasseAttendu = "secret123";
+let motDePasseUtilisateur = "1234"; 
+if ( motDePasseUtilisateur ===  motDePasseAttendu ) {
+    console.log("Accès autorisé. Bienvenue !");
+} else if ( motDePasseUtilisateur.length < 8  ) {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}
+
+
